7ab555489efc4b1e1f20043f2c3348a1de5116db
Scorpio Software <scorpiosoftware666@gmail.com>
2024-09-21 20:22:09 +0300

    signup 21-09-2024 08:22 PM

--- resources/views/livewire/sign-up.blade.php
+++ resources/views/livewire/sign-up.blade.php
@@ -21,14 +21,14 @@
                             class="block mb-2 text-sm font-medium text-gray-900 ">name</label>
                         <input type="text" id="name" name="name"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="John" required />
+                            placeholder="" required />
                     </div>
                     <div class="mb-6">
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email
                             address</label>
                         <input type="email" id="email" name="email"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="john.doe@company.com" required />
+                            placeholder="" required />
                     </div>
                 </div>
 
