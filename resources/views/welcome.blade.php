82d20d35baf495bfc0f0f57c548393c8a5735736
Scorpio Software <scorpiosoftware666@gmail.com>
2024-09-22 15:34:13 +0300

    22-08-2024 3:34 pm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     {{-- @vite('resources/css/app.css', 'resources/js/app.js') --}}
@@ -34,8 +35,14 @@
             font-weight: 500;
             font-style: normal;
         }
+
+        .poppins-regular {
+            font-family: "Poppins", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
     </style>
-    <div class="">
+    <div class="poppins-regular">
         @guest
             <livewire:sign-up>
             @endguest
@@ -44,20 +51,20 @@
                     <livewire:news-lable>
                         <livewire:navbar>
                             @foreach ($carousels as $carousel)
-                            <livewire:carousel :record='$carousel'  url='carousel-2.jpg' title='UNIQUE DRESSES'
-                            position='top-[40%] right-1/3 z-40 translate-x-1/2 translate-y-1/2'>
+                                <livewire:carousel :record='$carousel' url='carousel-2.jpg' title='UNIQUE DRESSES'
+                                    position='top-[40%] right-1/3 z-40 translate-x-1/2 translate-y-1/2'>
                             @endforeach
                             {{-- <livewire:carousel :record='$carousel1'  url='carousel-2.jpg' title='UNIQUE DRESSES'
                                 position='top-[40%] right-1/3 z-40 translate-x-1/2 translate-y-1/2'> --}}
-                                <livewire:category-scroll>
-                                    {{-- <livewire:carousel :record='$carousel2' url='carousel-1.jpg' title='LUXURY AHAYAS'
+                            <livewire:category-scroll>
+                                {{-- <livewire:carousel :record='$carousel2' url='carousel-1.jpg' title='LUXURY AHAYAS'
                                         position='top-[40%] left-20 z-40 translate-y-1/2'> --}}
-                                        <livewire:categorywithimage lazy>
-                                            <livewire:product-slider>
-                                                <livewire:post-section :posts='$posts'>
-                                                    <livewire:footer>
-                                                        <x-home.speed-dial />
-                                                        <livewire:top>
+                                <livewire:categorywithimage lazy>
+                                    <livewire:product-slider>
+                                        <livewire:post-section :posts='$posts'>
+                                            <livewire:footer>
+                                                <x-home.speed-dial />
+                                                <livewire:top>
     </div>
     </div>
     <x-section.scripts />
